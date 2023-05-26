3ea7f91b2d45eb9edd93dfb6b660a3ef3985db35
Sanjar <sanjarovs123@gmail.com>
2023-05-26 14:10:44 +0500

    Прописан админ сидер

--- database/seeders/Admin.php
+++ database/seeders/Admin.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class Admin extends Seeder
 {
@@ -12,6 +14,12 @@ class Admin extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name'           => 'Admin',
+            'email'          => 'admin@gmail.com',
+            'password'       => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'is_admin'       => 1
+        ]);
     }
 }
