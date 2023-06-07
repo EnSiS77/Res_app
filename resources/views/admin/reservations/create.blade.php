db0479201de1bfebe027dc9b5c826e64e2572da3
Sanjar <sanjarovs123@gmail.com>
2023-06-07 15:58:42 +0500

    Просисано сохранение и верстка бронирования

--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -14,31 +14,71 @@
 
             <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form enctype="multipart/form-data">
-                        <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700"> Имя </label>
+
+                    <form method="POST" action="{{ route('admin.reservation.store') }}">
+                        @csrf
+                        <div class="sm:col-span-6 mt-2">
+                            <label for="first_name" class="block text-sm font-medium text-gray-700">Имя</label>
                             <div class="mt-1">
-                                <input type="text" id="title" wire:model.lazy="title" name="title"
-                                    class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="text" id="first_name" name="first_name"
+                                    class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
-                        <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700">Фото </label>
+                        <div class="sm:col-span-6 mt-2">
+                            <label for="last_name" class="block text-sm font-medium text-gray-700"> Фамилия </label>
                             <div class="mt-1">
-                                <input type="file" id="image" wire:model="newImage" name="image"
-                                    class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="text" id="last_name" name="last_name"
+                                    class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 mt-2">
+                            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                            <div class="mt-1">
+                                <input type="email" id="email" name="email"
+                                    class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 mt-2">
+                            <label for="tel_number" class="block text-sm font-medium text-gray-700">Номер
+                                телефона</label>
+                            <div class="mt-1">
+                                <input type="tel" id="tel_number" name="tel_number"
+                                    class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 mt-2">
+                            <label for="res_date" class="block text-sm font-medium text-gray-700">Дата
+                                резервации</label>
+                            <div class="mt-1">
+                                <input type="datetime-local" id="res_date" name="res_date"
+                                    class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 mt-2">
+                            <label for="guest_number" class="block text-sm font-medium text-gray-700" >Количество гостей</label>
+                            <div class="mt-1">
+                                <input type="number" id="guest_number" name="guest_number" min="1"
+                                    class="block w-full  appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
 
-                        <div class="sm:col-span-6 pt-5">
-                            <label for="body" class="block text-sm font-medium text-gray-700">Описание</label>
+                        <div class="sm:col-span-6 mt-3">
+                            <label for="table" class="block text-sm font-medium text-gray-700">Столик</label>
                             <div class="mt-1">
-                                <textarea id="body" rows="3" wire:model.lazy="body"
-                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                <select id="table_id" name="table_id"
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 form-multiselect">
+
+                                    @foreach ($tables as $table)
+                                        <option value="{{ $table->id }}">{{ $table->name }}</option>
+                                    @endforeach
+
+                                </select>
                             </div>
                         </div>
+
                         <div class="px-6 py-4">
-                            <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Сохранить</button>
+                            <button type="submit"
+                                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Сохранить</button>
                         </div>
                     </form>
                 </div>
--- resources/views/admin/tables/index.blade.php
+++ resources/views/admin/tables/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-end m-2 p-2">
                 <a href="{{ route('admin.tables.create') }}"
-                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Добавить блюдо в меню</a>
+                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Добавить столик</a>
             </div>
 
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
