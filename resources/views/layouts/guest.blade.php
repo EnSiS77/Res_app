f6504fd03b77e22d44f01fc544844c3fc9bf81b0
Sanjar <sanjarovs123@gmail.com>
2023-06-14 15:02:24 +0500

    Созданы  контроллеры и роуты для  главной страницы

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -40,13 +40,15 @@
               <div :class="isOpen ? 'flex' : 'hidden'"
                 class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                  href="#">Главная</a>
+                  href="/">Главная</a>
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                  href="#">Про нас</a>
+                  href="">Про нас</a>
+                  <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+                  href="{{ route('categories.index') }}">Категории блюд</a>
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                  href="#">Наше меню</a>
+                  href="{{ route('menus.index') }}">Наше меню</a>
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                  href="#">Забронировать столик</a>
+                  href="{{ route('reservations.step.one') }}">Забронировать столик</a>
                 <div>
                   
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,25 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\ReservationController;
 use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Frontend\CategoryController as FrontendCategoryController;
+use App\Http\Controllers\Frontend\MenuController as FrontendMenuController;
+use App\Http\Controllers\Frontend\ReservationController as FrontendReservationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/categories', [FrontendCategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/{category}', [FrontendCategoryController::class, 'show'])->name('categories.show');
+Route::get('/menu', [FrontendMenuController::class, 'index'])->name('menus.index');
+Route::get('/reservation/step-one', [FrontendReservationController::class, 'stepOne'])->name('reservations.step.one');
+Route::get('/reservation/step-two', [FrontendReservationController::class, 'stepTwo'])->name('reservations.step.two');
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -21,6 +34,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function() {
     Route::get('/', [AdminController::class, 'index'])->name('index');
