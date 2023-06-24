b54dbdac8063fe2bd303203b3dd60d427e7f2e7c
sanjar <sanjarovs123@gmail.com>
2023-06-24 15:33:50 +0500

    Попробовал сделать ауз из дашборд добавть в индекс

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -54,12 +54,19 @@
                     href="{{ route('reservations.step.one') }}">Забронировать столик</a>
 
                     <div class="text-end">
-                      <a href="/login">
-                        <button type="button" class="btn btn-outline-light me-2">Login</button>
-                      </a>
-                      <a href="/register">
-                        <button type="button" class="btn btn-warning">Sign-up</button>
-                      </a>
+                        @if (Route::has('login'))
+                        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                            @auth
+                                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                            @else
+                                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+        
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                                @endif
+                            @endauth
+                        </div>
+                    @endif
                     </div>
             </div>
 
